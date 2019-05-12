921119483d531b2efea5023ec21d4ba362641cd3
Daniel Mendes <daniel.infosistemas@gmail.com>
2019-05-12 20:56:06 -0300

    Carousel Editor and updating forms

--- src/app/administracao/carousel/carousel.component.ts
+++ src/app/administracao/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertModalService } from 'src/app/shared/alert-modal.service';
 
 @Component({
   selector: 'app-carousel',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  selectedFile: File = null;
+
+  imagename: string = "";
+
+  imagepath: any = [];
+
+  constructor(private alertModal: AlertModalService) { }
 
   ngOnInit() {
+  }
+
+  onSelectedFile(event: any) {
+    this.selectedFile = <File>event.target.files[0];
+    this.imagename = this.selectedFile.name;
+  }
+
+  addUrl() {
+    if (this.selectedFile !== null) {
+      this.imagepath.push(
+        {
+          name: this.selectedFile.name,
+          url: this.selectedFile,
+        }
+      );
+      this.selectedFile = null;
+      this.imagename = "";
+    } else {
+      this.alertModal.showAlertSuccess('Favor selecionar uma imagem.', 'Atenção!');
+    }
+  }
+
+  trashUrl(i: number) {
+    this.imagepath.splice(i, 1);
+  }
+
+  submitUrl() {
+    console.log(this.imagepath);
+    this.imagepath = [];
+    this.alertModal.showAlertSuccess('Carroussel atualizado com sucesso.', 'Atenção!');
+  }
+
+  cancelUrl() {
+    if (this.imagepath.length > 0) {
+      this.imagepath = [];
+    } else {
+      this.alertModal.showAlertSuccess('Não existem imagens na lista.', 'Atenção!');
+    }
   }
 
 }
